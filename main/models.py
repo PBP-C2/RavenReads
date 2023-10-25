eb47c3df43302e867ec45c98e4a65e63eaa84cfb
nandikarafi <nandikarafi@gmail.com>
2023-10-25 21:52:14 +0700

    tes

--- main/models.py
+++ main/models.py
@@ -28,3 +28,6 @@ class Thread(models.Model):
     images = models.ImageField(upload_to='images/')
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+
+class Books(models.Model):
+    
