f7c6e37426b6d4d81aa0e324c12a6d0932cdecfb
Haramchu <clementsamuelm@gmail.com>
2023-10-27 20:13:57 +0700

    remove ekstra import

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from main.models import Person, MainThread, Thread, QuizPoint
+from main.models import Person, MainThread, Thread
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
