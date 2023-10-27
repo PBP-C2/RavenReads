437a9047d0fd0d0d563115587d37bd296545de4b
fikririsyad <fikri.risyad10@gmail.com>
2023-10-27 16:30:14 +0700

    Nambahin review sama modalnya

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from main.views import (book_progression, book_store, forum_discussion,
                         get_reading_progress, import_books_from_csv,
                         login_user, logout_user, make_thread, open_main_thread,
-                        register, reply, show_main)
+                        register, reply, show_main, increment_progress, add_review, get_reading_progress_by_id)
 
 app_name = 'main'
 
@@ -18,6 +18,9 @@ urlpatterns = [
     path('discussion/<int:id>/reply/', reply, name='reply'),
     path('book-progression/', book_progression, name='book_progression'),
     path('get-progression/', get_reading_progress, name='get_reading_progress'),
+    path('get-progression/<int:id>/', get_reading_progress_by_id, name='get_reading_progress_by_id'),
+    path('increment-progress/<int:id>/', increment_progress, name='increment_progress'),
+    path('add-review/<int:id>/', add_review, name='add_review'),
     path('book_store/', book_store, name='book_store'),
     path('import_books_from_csv/',import_books_from_csv, name='import_books_from_csv')
 ]
