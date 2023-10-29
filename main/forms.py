ac1e3aa0b0f34f807aa0de05903d843c0b843507
Rizki Maulana <rizki.maulana23@ui.ac.id>
2023-10-29 15:47:07 +0700

    changes

--- main/forms.py
+++ main/forms.py
@@ -5,10 +5,23 @@ from django.contrib.auth.models import User
 from django import forms
 
 class PersonForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PersonForm, self).__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['name'].widget.attrs['placeholder'] = 'Name'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['tipe'].widget.attrs['class'] = 'form-control tipe'
+        self.fields['tipe'].widget.attrs['placeholder'] = 'Tipe'
+        self.fields['tipe'].widget.attrs['readonly'] = True
+        self.fields['gender'].widget.attrs['class'] = 'form-control'
+
     GENDER = (
         ('Male', 'male'),
         ('Female', 'female'),
     )
+    
 
     gender = forms.ChoiceField(choices=GENDER)
     class Meta:
@@ -27,15 +40,16 @@ class ThreadForm(ModelForm):
         fields = ["content"]
 
 class UserForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm password'
+
     class Meta:
         model = User
         fields = ["username", "password1", "password2"]
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control',
-                                                'placeholder': 'Password',
-                                                'required': 'required'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control', 
-                                                    'placeholder': 'Password',
-                                                    'required': 'required'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
-        }
